28dc48bbcdf5018532801acc6a454c8e1ddd708a
VISHAL Subramanian <vishalsubbu97@gmail.com>
2018-08-24 12:56:49 +0530

    Update conc_1D.m

--- phase_field_kobayashi/conc_1D.m
+++ phase_field_kobayashi/conc_1D.m
@@ -1,3 +1,13 @@
+%
+% Copyright (c) 2018, Vishal_S
+% All rights reserved. Please read the "license.txt" for license terms.
+%
+% Project Title: phase_field_kobayashi
+% 
+% Developer: Vishal S
+% 
+% Contact Info: vishalsubbu97@gmail.com
+%
 clear all
 clc
 clf
