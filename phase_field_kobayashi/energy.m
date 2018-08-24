61ea85e73e2b2a7e7bb1b28fef362bc97f41d114
VISHAL Subramanian <vishalsubbu97@gmail.com>
2018-08-24 12:57:55 +0530

    Update energy.m

--- phase_field_kobayashi/energy.m
+++ phase_field_kobayashi/energy.m
@@ -1,3 +1,13 @@
+%
+% Copyright (c) 2018, Vishal_S
+% All rights reserved. Please read the "license.txt" for license terms.
+%
+% Project Title: phase_field_kobayashi
+% 
+% Developer: Vishal S
+% 
+% Contact Info: vishalsubbu97@gmail.com
+%
 p = -0.2:0.01:1.2;
 alpha = 0.9;
 gamma = 10.0;
