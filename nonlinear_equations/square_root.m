83ecf466e6e030e4a91d8b3907b7f51093efe825
VISHAL Subramanian <vishalsubbu97@gmail.com>
2018-08-24 12:52:15 +0530

    Update square_root.m

--- nonlinear_equations/square_root.m
+++ nonlinear_equations/square_root.m
@@ -1,3 +1,13 @@
+%
+% Copyright (c) 2018, Vishal_S
+% All rights reserved. Please read the "license.txt" for license terms.
+%
+% Project Title: nonlinear_equations
+% 
+% Developer: Vishal S
+% 
+% Contact Info: vishalsubbu97@gmail.com
+%
 converge = 1.0;
 initial_guess = 1.0;
 a = 2.34886276;
