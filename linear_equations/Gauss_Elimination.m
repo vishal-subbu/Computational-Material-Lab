8543550cb7da244f3d05f7c3388aa5d571e5adb0
VISHAL Subramanian <vishalsubbu97@gmail.com>
2018-08-24 12:50:04 +0530

    Update Gauss_Elimination.m

--- linear_equations/Gauss_Elimination.m
+++ linear_equations/Gauss_Elimination.m
@@ -1,4 +1,13 @@
-
+%
+% Copyright (c) 2018, Vishal_S
+% All rights reserved. Please read the "license.txt" for license terms.
+%
+% Project Title: Linear_equations
+% 
+% Developer: Vishal S
+% 
+% Contact Info: vishalsubbu97@gmail.com
+%
 
 N = 8;
 A = zeros(N);
